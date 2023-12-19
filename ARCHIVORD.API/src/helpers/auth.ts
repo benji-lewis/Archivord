635172c5f0d98e013f81865065dc47db914fc11c
Ben Lewis <benji@benjilewis.dev>
2023-12-19 19:34:46 +0000

    Checks discord token correctly

--- ARCHIVORD.API/src/helpers/auth.ts
+++ ARCHIVORD.API/src/helpers/auth.ts
@@ -1,4 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
+import type { archivord } from '../index.d';
+import type { RequestHandler } from 'express';
 
 /**
  * Checks a provided discord token is valid
@@ -9,29 +11,29 @@ import axios from "axios";
  *
  * @return {Promise<boolean>} - Bool indicating if the token is valid
  */
-async function checkDiscordToken(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    res.status(401).send("Unauthorized");
-    return;
-  }
+export const checkDiscordToken: RequestHandler = async (req: archivord.ReqUserInfo, res, next) => {
+	const authHeader = req.headers.authorization;
+	if (!authHeader || !authHeader.startsWith('Bearer ')) {
+		res.status(401).send('Unauthorized');
+		return;
+	}
 
-  const token = authHeader.split(" ")[1];
-  try {
-    const response = await axios.get("https://discord.com/api/oauth2/@me", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
+	const token = authHeader.split(' ')[1];
+	try {
+		const response = await axios.get('https://discord.com/api/oauth2/@me', {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
 
-    if (response.status !== 200) {
-      res.status(401).send("Invalid Discord token");
-      return;
-    }
+		if (response.status !== 200) {
+			res.status(401).send('Invalid Discord token');
+			return;
+		}
 
-    req.user = response.data;
-    next();
-  } catch (error) {
-    res.status(500).send("Error verifying Discord token");
-  }
-}
+		req.user = response.data;
+		next();
+	} catch (error) {
+		res.status(500).send('Error verifying Discord token');
+	}
+};
