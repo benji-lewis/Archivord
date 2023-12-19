9fa87d47f722ac660d732a2cdf0bbe6f5ee0e8a6
Ben Lewis <benji@benjilewis.dev>
2023-12-19 00:05:02 +0000

    Initial backup functionality is working

---
+++ ARCHIVORD.Bot/src/index.d.ts
@@ -0,0 +1,32 @@
+import { Timestamp } from "firebase-admin/firestore";
+
+export namespace archivord {
+	interface Guilds {
+		[guildId: string]: Guild;
+	}
+
+	interface Guild {
+		guildName: string;
+		channels?: {
+			[channelId: string]: Channel;
+		};
+	}
+
+	interface Channel {
+		channelName: string;
+		roles: string[];
+		messages?: Messages;
+	}
+
+	interface Messages {
+		[messageId: string]: Message;
+	}
+
+	interface Message {
+		content: string;
+		authorId: string;
+		authorUsername: string;
+		authorNick?: string;
+		timestamp: Timestamp;
+	}
+}
