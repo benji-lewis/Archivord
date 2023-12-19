b46b0ba5c0b3f12edf6738f24f7bff0d5e5cd7a9
Ben Lewis <benji@benjilewis.dev>
2023-12-19 19:02:10 +0000

    Added discord auth verification helper function

---
+++ ARCHIVORD.API/src/helpers/auth.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+/**
+ * Checks a provided discord token is valid
+ *
+ * @param {Request} req - Express request object
+ * @param {Response} res - Express response object
+ * @param {NextFunction} next - Express next function
+ *
+ * @return {Promise<boolean>} - Bool indicating if the token is valid
+ */
+async function checkDiscordToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  const token = authHeader.split(" ")[1];
+  try {
+    const response = await axios.get("https://discord.com/api/oauth2/@me", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (response.status !== 200) {
+      res.status(401).send("Invalid Discord token");
+      return;
+    }
+
+    req.user = response.data;
+    next();
+  } catch (error) {
+    res.status(500).send("Error verifying Discord token");
+  }
+}
