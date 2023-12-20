972b932f78c10c46ce035653421c0f24f2c09820
Ben Lewis <benji@benjilewis.dev>
2023-12-20 14:58:29 +0000

    Removed 3rd party CORS module

--- ARCHIVORD.API/src/index.ts
+++ ARCHIVORD.API/src/index.ts
@@ -1,10 +1,12 @@
-import {onRequest} from 'firebase-functions/v2/https';
+import { onRequest } from 'firebase-functions/v2/https';
+// Available log levels: log, info, debug, warn, error
+// import { debug } from 'firebase-functions/logger';
 
 import express = require('express');
-import cors = require('cors');
+import { checkDiscordToken } from './helpers/auth';
 const app = express();
 
-app.use(cors());
+app.use(checkDiscordToken);
 
 app.get('/guilds', (req, res) => {
 	const dummyData = ['624200301770965043', '624200301770965044', '624200301770965045'];
