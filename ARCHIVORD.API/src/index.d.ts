8d0cb3a303f5f1e3da9f55e1edad676912b962db
Benjamin Lewis <ben.lewis@visimedia.co.uk>
2023-12-25 19:06:08 +0000

    Added API types

--- ARCHIVORD.API/src/index.d.ts
+++ ARCHIVORD.API/src/index.d.ts
@@ -1,4 +1,5 @@
 import { Request } from 'express';
+import { Timestamp } from 'firebase-admin/firestore';
 
 export namespace archivord {
 	interface ReqUserInfo extends Request {
@@ -25,5 +26,40 @@ export namespace archivord {
 		locale: string,
 		email: string,
 		verified: boolean
+	}
+
+	interface Guilds {
+		[guildId: string]: Guild;
+	}
+
+	interface Guild {
+		guildName: string;
+		icon: string;
+		channels?: {
+			[channelId: string]: Channel;
+		};
+	}
+
+	interface Channels {
+		[channelId: string]: Channel;
+	}
+
+	interface Channel {
+		channelName: string;
+		topic: string;
+		roles?: string[];
+		messages?: Messages;
+	}
+
+	interface Messages {
+		[messageId: string]: Message;
+	}
+
+	interface Message {
+		content: string;
+		authorId: string;
+		authorUsername: string;
+		authorNick?: string;
+		timestamp: Timestamp;
 	}
 }
