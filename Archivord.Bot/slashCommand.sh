567df8ebae6ef1d82eaf739e3f246b9bb39b4a5b
Benji Lewis <benji@benjilewis.dev>
2023-12-26 23:04:05 +0000

    Revert "Removed capitalisation from directories"
    This reverts commit 5eab3f6e5ab9c0931f79902d1c8d98c83f06d27f.

--- Archivord.Bot/slashCommand.sh
+++
@@ -1,9 +0,0 @@
-curl --request POST \
-  --url https://discord.com/api/v10/applications/---CLIENT ID GOES HERE---/commands \
-  --header 'Authorization: Bot ---BOT TOKEN GOES HERE---' \
-  --header 'Content-Type: application/json' \
-  --data '{
-    "name": "backup",
-    "type": 1,
-    "description": "Starts a backup in this channel"
-}'
