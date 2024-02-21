27fedc5ce18faecb6c170886badca1e8a18dfa4a
Ben Lewis <benji@benjilewis.dev>
2024-02-21 20:17:09 +0000

    📝 Updated Home page UI
    🔗 I've added a Link import from @mui/material.
    💬 Adjusted the Typography elements to include a new message about work in progress and a link to report issues on GitHub.

--- Archivord.UI/src/pages/Home.tsx
+++ Archivord.UI/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material"
+import { Box, Link, Typography } from "@mui/material"
 
 export const Home = () => {
   return (
@@ -12,8 +12,9 @@ export const Home = () => {
         textAlign: 'center'
       }}
     >
-      <Typography variant="h1">Archivord</Typography>
       <Typography variant="h4">An archival bot for Discord servers designed to retain as much data as possible to show future generations how we communicated</Typography>
+      <Typography variant="h5">This is a work in progress. Please be patient.</Typography>
+      <Typography paragraph>Please report any issues on <Link color={"#fffff"} href="https://github.com/benjisoft/Archivord">our GitHub repo</Link>.</Typography>
     </Box>
   )
 }
