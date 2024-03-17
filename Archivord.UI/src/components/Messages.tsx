11555469630267e785bd5f07ae7897de567325bb
Ben Lewis <benji@benjilewis.dev>
2024-03-17 16:25:25 +0000

    Improved message scrolling and debugging
    🔄 Imported useEffect, useRef from React for better control over component rendering.
    📜 Implemented auto-scrolling to the latest message in chat using a reference div.
    🔍 Added console logs for detailed debugging of authorNick, authorUsername, and content properties.
    ⚙️ Fixed missing key prop issue in DiscordMessage component.

--- Archivord.UI/src/components/Messages.tsx
+++ Archivord.UI/src/components/Messages.tsx
@@ -1,6 +1,7 @@
 import { styled } from "@mui/material"
 import { Message, MessageData } from "../interfaces/Message"
 import { DiscordMessages, DiscordMessage } from "@skyra/discord-components-react"
+import { useEffect, useRef } from 'react';
 
 const Box = styled('div', {})(() => ({
   width: "100% !important",
@@ -13,18 +14,29 @@ const Box = styled('div', {})(() => ({
 }))
 
 export const Messages = ({ messages }: { messages: Message }) => {
+  const endRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    endRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages]);
+
   return (
     <Box>
-    <DiscordMessages noBackground>
-      {Object.entries(messages).map(([key, value]: [string, MessageData]) => (
-        <DiscordMessage
-          author={value.authorNick ? value.authorNick : value.authorUsername}
-        >
-          {value.content}
-        </DiscordMessage>
-      ))}
-
-    </DiscordMessages>
+      <DiscordMessages noBackground>
+        {Object.entries(messages).map(([key, value]: [string, MessageData]) => {
+          console.log(`**** Nick: ${value.authorNick} - Nick Type: ${typeof value.authorNick} - Username: ${value.authorUsername} - Content: ${value.content}`);
+          console.log(`**** Message: ${value.content}`)
+          return (
+            <DiscordMessage
+              key={key} // Turns out this is actually needed... That's 3 hours of my life I won't get back
+              author={value.authorNick || value.authorUsername}
+            >
+              {value.content}
+            </DiscordMessage>
+          );
+        })}
+        <div ref={endRef} /> {/* This div will be scrolled into view */}
+      </DiscordMessages>
     </Box>
   )
 }
